f0e65b851f690eabda2ca265b7e98d52f1c9bb8d
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2023-09-18 16:43:54 -0500

    updated MVC and css to display mock

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
 
-router.use('/users', userRoutes);
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
+
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,30 +1,25 @@
-const signupFormHandler = async function (event) {
+const signupFormHandler = async function(event) {
   event.preventDefault();
 
-  const username = document
-    .querySelector("#username-input-signup")
-    .value.trim();
-  const password = document
-    .querySelector("#password-input-signup")
-    .value.trim();
+  const username = document.querySelector('#username-input-signup').value.trim();
+  const password = document.querySelector('#password-input-signup').value.trim();
   console.log(username);
   console.log(password);
-  const response = await fetch("/api/user", {
-    method: "POST",
+  const response = await fetch('/api/user', {
+    method: 'POST',
     body: JSON.stringify({
-      username,
-      password,
+      username, password
     }),
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace("/");
+    document.location.replace('/dashboard');
   } else {
-    alert("Failed to sign up");
+    alert('Failed to sign up');
   }
 };
 
 document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
